688bb3a144322d3e420bde619de084d5106e09a6
Ben Wicker <bwicker@cedarville.edu>
2019-07-24 15:26:46 -0400

    More layout stuff

--- pages/species.vue
+++ pages/species.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-md fluid>
     <v-layout justify-center column fill-height>
-      <v-flex xs10>
+      <v-flex xs11>
         <v-layout row class="fill-height">
           <v-flex xs4>
             <v-card dark color="primary" class="fill-height">
@@ -9,9 +9,18 @@
             </v-card>
           </v-flex>
           <v-flex xs4>
-            <v-card dark color="primary" class="fill-height">
-              <v-card-text class="px-0">12</v-card-text>
-            </v-card>
+            <v-layout column class="fill-height">
+              <v-flex xs7>
+                <v-card dark color="primary" class="fill-height">
+                  <v-card-text class="">12</v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs5>
+                <v-card dark color="primary" class="fill-height">
+                  <v-card-text class="">12</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
           </v-flex>
           <v-flex xs4>
             <v-card dark color="primary" class="fill-height">
@@ -20,10 +29,24 @@
           </v-flex>
         </v-layout>
       </v-flex>
-      <v-flex xs2>
-        <v-card dark color="secondary" class="fill-height">
-          <v-card-text class="px-0">12</v-card-text>
-        </v-card>
+      <v-flex xs1>
+        <v-layout row class="fill-height">
+          <v-flex xs2>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs8>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs2>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
