71143232088a08e463679466c0d9be66960ef0f6
Ben Wicker <bwicker@cedarville.edu>
2019-07-24 14:16:48 -0400

    Basic layout present for species selection

--- pages/species.vue
+++ pages/species.vue
@@ -1,19 +1,46 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;Choose species here.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-container grid-list-md fluid>
+    <v-layout justify-center column fill-height>
+      <v-flex xs10>
+        <v-layout row class="fill-height">
+          <v-flex xs4>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs4>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs4>
+            <v-card dark color="primary" class="fill-height">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="secondary" class="fill-height">
+          <v-card-text class="px-0">12</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import HoloIcon from "~/components/HoloIcon.vue";
+
+export default {
+  components: {
+    HoloIcon
+  }
+};
+</script>
+
+<style scoped>
+.item {
+  margin: 10px;
+}
+</style>
