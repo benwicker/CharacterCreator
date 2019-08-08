dd2eb080e13ea08b627e3123bf32a5af255bb2b1
Ben Wicker <bwicker@cedarville.edu>
2019-08-08 11:47:54 -0400

    Decided on vertical tabs since can't get normal tabs working

--- components/class/Class.vue
+++ components/class/Class.vue
@@ -1,22 +1,48 @@
 <template>
   <v-container grid-list-md pa-0 ma-0>
-      <v-tabs dark vertical class="fill-height">
-          <v-tab class="fill-height">Overview</v-tab>
+    <v-layout column>
+      <v-flex sm11>
+        <v-tabs dark vertical v-model="tab" class="fill-height">
+          <v-tab>Overview</v-tab>
           <v-tab>Features</v-tab>
-          <v-tab>Subclasses</v-tab>
-          <v-tab-item class="fill-height primary">
-              <div class="fill-height">Tab 1 item</div>
-          </v-tab-item>
-          <v-tab-item>
-              <div>Tab 2 item</div>
-          </v-tab-item>
-          <v-tab-item>
-              <div>Tab 3 item</div>
-          </v-tab-item>
-      </v-tabs>
+          <v-tab>SubClasses</v-tab>
+          <v-tab>Powers</v-tab>
+
+          <v-tabs-items class="fill-height"  v-model="tab">
+            <v-tab-item class="fill-height">
+              <div>Hello1</div>
+            </v-tab-item>
+            <v-tab-item class="fill-height">
+              <div>Hello2</div>
+            </v-tab-item>
+            <v-tab-item class="fill-height">
+              <div>Hello3</div>
+            </v-tab-item>
+            <v-tab-item class="fill-height">
+              <div>Hello4</div>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-tabs>
+      </v-flex>
+      <v-flex sm1>
+          <div>Hello</div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tab: null
+    };
+  }
+};
 </script>
+
+<style scoped>
+    .v-tabs-items >>> .v-window__container {
+        height: 100%;
+    }
+</style>
